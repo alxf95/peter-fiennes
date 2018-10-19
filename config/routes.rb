4f973cbc41e788e2b3eb5fb52de8ce210ea518fb
Alex Fiennes <alexfiennes246@gmail.com>
2018-10-19 20:37:41 +0100

    Pretty much done but some final touches should be made

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
-  get 'books', to: 'pages#books'
-  get 'media', to: 'pages#media'
-  get 'contact', to: 'pages#contact'
+  get 'contact-me', to: 'messages#new', as: 'new_message'
+  post 'contact-me', to: 'messages#create', as: 'create_message'
 end
